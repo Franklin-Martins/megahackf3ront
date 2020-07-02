a7b928481c0b0ef21ee1d7487f71b7fd0d60e87f
Gabriel de Jesus <gabrieeldejesus@hotmail.com>
2020-07-02 10:21:15 -0300

    upload of images and lading page header created

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,21 @@ import './style.css'
 export default function Home(){
     return (
         <div id='home'>
-            Home
+            <header>
+                <div class="container">
+                    <div class="logo"><a href="#"><img src="assets/logomarca.svg" alt="logomarca"/></a></div>
+                    <div class="menu">
+                        <nav>
+                            <ul>
+                                <li><a href="#">Início</a></li>
+                                <li><a href="#">Prepare a sua escola</a></li>
+                                <li><a href="#">Contato</a></li>
+                                <li><div class="button"><a href="#">Quero na minha escola</a></div></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
         </div>
     );
 }
