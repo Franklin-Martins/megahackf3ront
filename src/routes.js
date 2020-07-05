22876d5213c6a25777887b63a8e7ee0c6926191e
Gabriel de Jesus <gabrieeldejesus@hotmail.com>
2020-07-05 15:54:03 -0300

    app start screen

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, BrowserRouter } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Initial from './pages/Inicio'
 
 function routes() {
     return (
       <BrowserRouter>
-      <Route component={Home} path="/" exact />
+        <Route component={Home} path="/" exact />
+        <Route component={Initial} path="/initial" />
       </BrowserRouter>
     );
   }
